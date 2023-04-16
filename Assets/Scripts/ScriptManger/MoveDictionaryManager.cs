1b8cefba8daa88a04afdd2cd90c73a1ce078e61b
manav907 <manav907@gmail.com>
2023-04-16 23:45:49 +0530

    Reorganized code Need to fix end turn bug

--- Assets/Scripts/ScriptManger/MoveDictionaryManager.cs
+++ Assets/Scripts/ScriptManger/MoveDictionaryManager.cs
@@ -9,7 +9,6 @@ public class MoveDictionaryManager : MonoBehaviour
     TurnManager turnManager;
     ReticalManager reticalManager;
     MapManager mapManager;
-    MoveDictionaryManager moveDictionaryManager;
     UniversalCalculator universalCalculator;
     ButtonManager buttonManager;
     public void setVariables()
@@ -29,65 +28,44 @@ public class MoveDictionaryManager : MonoBehaviour
         //Debug.Log(thisCharacter.name);//
         thisCharacterCDH = thisCharacter.GetComponent<CharacterControllerScript>();
     }
-    Dictionary<AbilityName, Action> ANtoA;
-
+    Dictionary<AbilityName, Action> abilityNameToAction;
     void SetMoveDictionary()
     {
-        ANtoA = new Dictionary<AbilityName, Action>();
-        ANtoA.Add(AbilityName.Move, MoveCharacter);
-        ANtoA.Add(AbilityName.Attack, AttackHere);
-        ANtoA.Add(AbilityName.EndTurn, endTurn);
-        ANtoA.Add(AbilityName.OpenInventory, OpenInventory);
-        ANtoA.Add(AbilityName.CloseInventory, CloseInventory);
-        ANtoA.Add(AbilityName.FireBall, ThrowFireBall);
-        ANtoA.Add(AbilityName.HeartPickup, HeartPickup);
+        abilityNameToAction = new Dictionary<AbilityName, Action>();
+        abilityNameToAction.Add(AbilityName.Move, MoveCharacter);
+        abilityNameToAction.Add(AbilityName.Attack, AttackHere);
+        abilityNameToAction.Add(AbilityName.EndTurn, endTurn);
+        abilityNameToAction.Add(AbilityName.OpenInventory, OpenInventory);
+        abilityNameToAction.Add(AbilityName.CloseInventory, CloseInventory);
+        abilityNameToAction.Add(AbilityName.FireBall, ThrowFireBall);
+        abilityNameToAction.Add(AbilityName.HeartPickup, HeartPickup);
+
+        //Co-RoutineStuff
+        IEnumerator StartSequence(List<IEnumerator> coroutines)
+        {
+            coroutines.Add(endTurnCoroutine());
+            foreach (IEnumerator coroutine in coroutines)
+            {
+                yield return StartCoroutine(coroutine);
+                yield return null;
+            }
+        }
+        //ablity Actions
         void MoveCharacter()
         {
-            List<IEnumerator> coroutines = new List<IEnumerator>()
-            {
-                getInput(simpleMoveAction,false,true,true,AbilityName.Move),
-                endTurnCoroutine()
-            };
+            var coroutines = new List<IEnumerator>();
+            coroutines.Add(getInput(simpleMoveAction, thisCharacterCDH.GetAbilityRange(AbilityName.Move), false, true));
             StartCoroutine(StartSequence(coroutines));
-        }
-        void simpleMoveAction()
-        {
-            Vector3Int currentPosition = universalCalculator.convertToVector3Int(thisCharacter.transform.position);
-            mapManager.cellDataDir[currentPosition].characterAtCell = null;
-            mapManager.cellDataDir[tryHere].characterAtCell = thisCharacter;
-            thisCharacter.transform.position = tryHere;
         }
         void AttackHere()
         {
-            List<IEnumerator> coroutines = new List<IEnumerator>()
-            {
-                getInput(simpleAttackAction,true,true||false,true,AbilityName.Attack),
-                endTurnCoroutine()
-            };
+            var coroutines = new List<IEnumerator>();
+            coroutines.Add(getInput(simpleAttackAction, thisCharacterCDH.GetAbilityRange(AbilityName.Attack), true, true || false));
             StartCoroutine(StartSequence(coroutines));
-        }
-        void simpleAttackAction()
-        {
-            CharacterControllerScript targetCharacter = mapManager.cellDataDir[tryHere].characterAtCell.GetComponent<CharacterControllerScript>();
-            CharacterControllerScript attackingCharacter = thisCharacter.GetComponent<CharacterControllerScript>();
-            targetCharacter.health -= attackingCharacter.AttackDamage;
-            checkCharacters(targetCharacter);
         }
         void endTurn()
         {
             StartCoroutine(endTurnCoroutine());
-
-        }
-        IEnumerator endTurnCoroutine()
-        {
-            yield return null;
-            CharacterControllerScript targetCharacter = thisCharacter.gameObject.GetComponent<CharacterControllerScript>();
-            targetCharacter.ToggleCharacterTurnAnimation(false); ;
-            this.GetComponent<TurnManager>().endTurn();
-        }
-        void checkCharacters(CharacterControllerScript targetCharacter)
-        {
-            targetCharacter.CheckIfCharacterIsDead();
         }
         void OpenInventory()
         {
@@ -105,25 +83,46 @@ public class MoveDictionaryManager : MonoBehaviour
         }
         void HeartPickup()
         {
-            //Debug.Log(thisCharacterCDH.health);
             thisCharacterCDH.health += 3;
             if (thisCharacterCDH.CheckIfCharacterIsDead() == false)
                 endTurn();
         }
-    }
-    IEnumerator StartSequence(List<IEnumerator> coroutines)
-    {
-        foreach (IEnumerator coroutine in coroutines)
+        //Simple Action and Co-routines that will be used for ablity Actions
+        void simpleMoveAction()
         {
-            yield return StartCoroutine(coroutine);
+            Vector3Int currentPosition = universalCalculator.convertToVector3Int(thisCharacter.transform.position);
+            mapManager.cellDataDir[currentPosition].characterAtCell = null;
+            mapManager.cellDataDir[tryHere].characterAtCell = thisCharacter;
+            thisCharacter.transform.position = tryHere;
+        }
+        void simpleAttackAction()
+        {
+            CharacterControllerScript targetCharacter = mapManager.cellDataDir[tryHere].characterAtCell.GetComponent<CharacterControllerScript>();
+            CharacterControllerScript attackingCharacter = thisCharacter.GetComponent<CharacterControllerScript>();
+            targetCharacter.health -= attackingCharacter.AttackDamage;
+            checkCharacters(targetCharacter);
+        }
+        IEnumerator endTurnCoroutine()
+        {
             yield return null;
+            CharacterControllerScript targetCharacter = thisCharacter.gameObject.GetComponent<CharacterControllerScript>();
+            targetCharacter.ToggleCharacterTurnAnimation(false); ;
+            this.GetComponent<TurnManager>().endTurn();
+        }
+        void checkCharacters(CharacterControllerScript targetCharacter)
+        {
+            targetCharacter.CheckIfCharacterIsDead();
         }
     }
-
-    IEnumerator getInput(Action doThisAction, bool gameObjectHere, bool walkableTileHere, bool needsButton, AbilityName abilityName)
+    public void doAction(AbilityName abilityName)
     {
-        //Debug.Log("Get Input Works");
-        listOfValidtargets = getValidTargetList(gameObjectHere, walkableTileHere, thisCharacterCDH.GetAbilityRange(abilityName));
+        abilityNameToAction[abilityName]();
+    }
+    Vector3Int tryHere;
+    IEnumerator getInput(Action doThisAction, int rangeOfAction, bool requireCharacter, bool requireWalkability)
+    {
+        bool needsButton = rangeOfAction == 0 ? false : true;
+        listOfValidtargets = getValidTargetList(requireCharacter, requireWalkability, rangeOfAction);
         if (thisCharacterCDH.isPlayerCharacter && needsButton)
             reticalManager.reDrawValidTiles(listOfValidtargets);//this sets the Valid Tiles Overlay
 
@@ -134,31 +133,35 @@ public class MoveDictionaryManager : MonoBehaviour
         }
         reticalManager.reDrawValidTiles(null);
         reticalManager.reDrawShadows();
+
+        bool GetDataForActions()
+        {
+            if (thisCharacterCDH.isPlayerCharacter)
+            {
+                tryHere = reticalManager.getMovePoint();
+                if (listOfValidtargets.Contains(tryHere))
+                    return true;
+                else if (listOfValidtargets.Count == 0)
+                {
+                    Debug.Log("No Valid Tiles Exist; Ending GetData");
+                    return false;
+                }
+                else
+                    return false;
+            }
+            else
+            {
+                tryHere = thisCharacterCDH.getTarget(listOfValidtargets);
+                return true;
+            }
+        }
     }
 
-    public void doAction(AbilityName abilityName)
-    {
-        ANtoA[abilityName]();
-    }
-    Vector3Int tryHere;
+
+
     List<Vector3Int> listOfValidtargets;
-    bool GetDataForActions()
-    {
-        if (thisCharacterCDH.isPlayerCharacter)
-        {
-            tryHere = reticalManager.getMovePoint();
-            if (listOfValidtargets.Contains(tryHere))
-                return true;
-            else
-                return false;
-        }
-        else
-        {
-            tryHere = thisCharacterCDH.getTarget(listOfValidtargets);
-            return true;
-        }
-    }
-    List<Vector3Int> getValidTargetList(bool GameObjectHere, bool WalkableTileHere, int rangeOfAction)
+    [SerializeField] bool debugValidActionTiles = false;
+    List<Vector3Int> getValidTargetList(bool requireCharacter, bool requireWalkability, int rangeOfAction)
     {
         //Debug.Log("Generating List of valid Targets for the character" + thisCharacter.name);
         Vector3Int centerPos = universalCalculator.convertToVector3Int(thisCharacter.transform.position);
@@ -169,37 +172,39 @@ public class MoveDictionaryManager : MonoBehaviour
         for (int i = 0; i < listOfRanges.Count; i++)
         {
             //Normal Checks         
-            bool isWalkableHere = mapManager.checkAtPosIfCharacterCanWalk(listOfRanges[i], thisCharacterCDH);
-            bool isGameObjectHere = mapManager.cellDataDir[listOfRanges[i]].isCellHoldingCharacer();
+            bool hasWalkability = mapManager.checkAtPosIfCharacterCanWalk(listOfRanges[i], thisCharacterCDH);
+            bool hasCharacter = mapManager.cellDataDir[listOfRanges[i]].isCellHoldingCharacer();
 
-            if (isWalkableHere == WalkableTileHere && isGameObjectHere == GameObjectHere)
+            if (hasWalkability == requireWalkability && hasCharacter == requireCharacter)
             {
                 //Debug.Log(listOfAttackRange[i] + "Valid " + i);
                 //Do Nothing since all conditions are fine
             }
             else
             {
-                //Debug.Log(listOfAttackRange[i] + "Invalid ");
-                listOfRanges.RemoveAt(i);
-                i--;
-                bool debugThis = false;
-                if (debugThis)
+                //For Debugging
+                if (debugValidActionTiles)
                 {
                     bool condtion = false;//Will be reassigned later
-                    string needConditon = (condtion) ? "Need " : "Does Not Need ";//Used to Concatinate String
-                    if (isWalkableHere != WalkableTileHere)
+                    string needConditon = (condtion) ? "Impossible Condition Occured for " : "Required ";//Used to Concatinate String
+                    string debugLine = "Point " + listOfRanges[i] + " was Invalid as Tile ";
+                    if (hasWalkability != requireWalkability)
                     {
-                        condtion = WalkableTileHere;
-                        //Debug.Log(needConditon + "Walkable Tile Here " + " but isWalkableHere is " + isWalkableHere);
-                        Debug.Log(needConditon + "Walkable Tile Here ");
+                        condtion = requireWalkability;
+                        debugLine += needConditon + "Ability to Walk ";
                     }
-                    if (isGameObjectHere != GameObjectHere)
+                    if (hasCharacter != requireCharacter)
                     {
-                        condtion = GameObjectHere;
-                        //Debug.Log(needConditon + "Game Object Here " + " but isGameObjectHere is " + isGameObjectHere);
-                        Debug.Log(needConditon + "Game Object Here ");
+                        condtion = requireCharacter;
+                        debugLine += needConditon + "Character Here ";
                     }
+                    if (rangeOfAction == 0)
+                        debugLine += "and The Range of Action was " + rangeOfAction;
+                    Debug.Log(debugLine);
                 }
+                //Actual Code 
+                listOfRanges.RemoveAt(i);
+                i--;
             }
         }
         return listOfRanges;
